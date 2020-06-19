4552ef4bdff87c3ec6076922f4156476b8e2b691
d-d-wolfe <dave.wolfe623@gmail.com>
2020-06-18 21:45:48 -0700

    made code look better

--- routes/api.js
+++ routes/api.js
@@ -5,29 +5,6 @@ const router = express.Router();
 
 const modelFinder = require('../middleware/model-finder.js')
 
-// const productSchema = require('../lib/models/products/products-schema');
-
-// const categorySchema = require('../lib/models/categories/categories-schema')
-
-// const Mongo = require('../lib/models/mongo.js');
-
-// function getModel(req, res, next) {
-//   let model = req.params.model;
-
-//   switch (model) {
-//     case 'products':
-//       req.model = new Mongo(productSchema);
-//       next();
-//       break;
-//       case 'categories':
-//         req.model = new Mongo(categorySchema);
-//         next();
-//         break;
-//         default:
-//           next('Wrong Model, dude!');
-//           break;
-//   }
-// }
 
 router.param('model', modelFinder);
 
